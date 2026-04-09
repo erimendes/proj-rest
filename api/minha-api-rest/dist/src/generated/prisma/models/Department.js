53e9058c3d743dc8b4f49ce860878c39ed181087
erimendes <eri.mendes32@mail.com>
2026-04-09 02:09:11 +0100

    Ajustes

--- api/minha-api-rest/dist/src/generated/prisma/models/Department.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Department.js.map
--- api/minha-api-rest/dist/src/user/entities/user.entity.js
+++
@@ -1,9 +0,0 @@
-import { createRequire as _createRequire } from "module";
-const __require = _createRequire(import.meta.url);
-const openapi = __require("@nestjs/swagger");
-export class User {
-    static _OPENAPI_METADATA_FACTORY() {
-        return {};
-    }
-}
-//# sourceMappingURL=user.entity.js.map
